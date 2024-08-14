bbc38d8e208312d6326437542781a6e1f19fd066
LuizF3lipe <luizf3lipe.dev@gmail.com>
2024-08-13 21:00:59 -0300

    create handler function postShortner

--- api/api.go
+++ api/api.go
@@ -1,21 +1,25 @@
 package api
 
 import (
+	"encoding/json"
+	"log/slog"
+	"math/rand/v2"
 	"net/http"
+	"net/url"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func NewHandler() http.Handler {
+func NewHandler(db map[string]string) http.Handler {
 	r := chi.NewMux()
 
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 
-	r.Post("/api/shorten", handlePostShortner)
-	r.Get("/{code}", handleGetShortner)
+	r.Post("/api/shorten", handlePostShortner(db))
+	r.Get("/{code}", handleGetShortner(db))
 
 	return r
 
@@ -30,10 +34,63 @@ type Response struct {
 	Data  any    `json:"data,omitempty"`
 }
 
-func handlePostShortner(w http.ResponseWriter, r *http.Request) {
+func handlePostShortner(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body PostBody
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			sendJson(w, Response{Error: "invalid json"}, http.StatusUnprocessableEntity)
+			return
+		}
 
+		_, err := url.Parse(body.URL)
+		if err != nil {
+			sendJson(w, Response{Error: "invalid url"}, http.StatusBadRequest)
+			return
+		}
+
+		code := genCode()
+		db[code] = body.URL
+		sendJson(w, Response{Data: code}, http.StatusCreated)
+	}
 }
 
-func handleGetShortner(w http.ResponseWriter, r *http.Request) {
+func handleGetShortner(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
+	}
+}
+
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func genCode() string {
+	const n = 8
+	byts := make([]byte, n)
+
+	for i := range n {
+		byts[i] = characters[rand.IntN(len(characters))]
+	}
+
+	return string(byts)
+}
+
+func sendJson(w http.ResponseWriter, resp Response, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("Failed to marshal response", "error", err)
+		sendJson(
+			w,
+			Response{
+				Error: "something went wrong",
+			},
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("Failed to write response to client", "error", err)
+		return
+	}
 }
